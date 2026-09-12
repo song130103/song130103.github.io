793ad61126b04bb9f11bdf6cc46356fe9d24a741
zreamer <972059313@qq.com>
2026-09-12 19:49:16 +0800

    Update script.js

--- zubuntu/script.js
+++ zubuntu/script.js
@@ -95,7 +95,7 @@ function start(o) {
 // 读取引导弹窗开关记录
 getItem('/usr/zdesktop/var/guide.var', res => {
     if (!(res == 1)) {
-        opn('http://song.zreamer.top/zubuntu/app/install/index.html');
+        wds.src='http://song.zreamer.top/zubuntu/app/install/index.html';
     }
 });
 
